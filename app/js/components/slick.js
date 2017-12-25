bf1b2d3e6d8c2c1dcc670be52583ffe8470d7ac3
Nelson119 <morphlina@gmail.com>
2017-12-25 09:11:30 +0800

    fc

---
+++ app/js/components/slick.js
@@ -0,0 +1,20 @@
+'use strict';
+/*eslint-disable new-cap, no-unused-vars,
+	no-use-before-define, no-trailing-spaces, space-infix-ops, comma-spacing,
+	no-mixed-spaces-and-tabs, no-multi-spaces, camelcase, no-loop-func,no-empty,
+	key-spacing ,curly, no-shadow, no-return-assign, no-redeclare, no-unused-vars,
+	eqeqeq, no-extend-native, quotes , no-inner-declarations*/
+/*global app, TweenMax*/
+app.partial.slick = function($, container){
+	container.on('page:update' , function(page, menu){
+        container.addClass('loaded');
+        $('.slick', container).slick({
+            arrows: false,
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            centerMode: true
+        });
+	});
+};
